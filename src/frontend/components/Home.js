8f271459773d5e258827806f2e5b4aceebdf362a
johnsmccain <johnsdanlami@gmail.com>
2024-06-26 08:44:04 +0100

    Manupulate Home Image component

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { ethers } from "ethers"
 import { Row, Col, Card, Button } from 'react-bootstrap'
+import Image from './Image'
 
 const Home = ({ marketplace, nft }) => {
   const [loading, setLoading] = useState(true)
@@ -58,6 +59,7 @@ const Home = ({ marketplace, nft }) => {
               <Col key={idx} className="overflow-hidden">
                 <Card>
                   <Card.Img variant="top" src={item.image}  />
+                  <Image url={item.image}/>
                   <Card.Body color="secondary">
                     <Card.Title>{item.name}</Card.Title>
                     <Card.Text>
--- src/frontend/components/Image.js
+++ src/frontend/components/Image.js
@@ -2,17 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
 import axios from 'axios';
 
-const Image = ({cid}) => {
+const Image = ({url}) => {
     const [imageUri, setImageUri] = useState();
     const [error, setError] = useState();
 
     const fetchImage = async () => {
 
         try {
-            let uri = `https://jade-just-rhinoceros-52.mypinata.cloud/ipfs/${cid}`
-            // let response = await fetch(uri);
-            console.log(uri)
-            let response = await axios.get(uri);
+
+            let response = await axios.get(url);
             if (!response.ok) throw new Error("Network response was not OK!");
 
             console.log(response)
